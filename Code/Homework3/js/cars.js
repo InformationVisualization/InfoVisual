b9bbdde57d8e7ac13257b381c0b20e66d8cd99e0
Kevin Novak <kevinmnovak@yahoo.com>
2017-03-14 22:42:57 -0400

    Fixed car visual

--- Code/Homework3/js/cars.js
+++ Code/Homework3/js/cars.js
@@ -1,4 +1,4 @@
-var svg = d3.select("#cars").append("svg"),
+var svg = d3.select("#carsSVG"),
     margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
