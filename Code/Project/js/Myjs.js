4549e3b8c994f27f3cb2d4f0bc2da04daf3f9d62
Harsh Patel <hpatel14@kent.edu>
2017-05-08 14:18:59 -0400

    Update Myjs.js

--- Code/Project/js/Myjs.js
+++ Code/Project/js/Myjs.js
@@ -144,7 +144,7 @@ map.on('draw:created', function (e) {
 		data = result;
 		DrawRS(result);
 
-		distVSspeed(result);
+		speedVSduration(result);
 		distVSduration(result);
 		initSpeedGroups(result);
 		initdurationGroups(result);
