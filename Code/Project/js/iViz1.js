3d08879a7c63118d8802afcc4744b414e0d6d08b
Harsh Patel <hpatel14@kent.edu>
2017-05-08 19:16:15 -0400

    Update iViz1.js

--- Code/Project/js/iViz1.js
+++ Code/Project/js/iViz1.js
@@ -3,61 +3,69 @@
 //05/08/2017
 //speedVSduration
 
-function round(value, decimals) {
-  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-}
-
-function barChart(avspeed, duration)
+function Circles(x, y, size)
 {
-  this.avspeed = avspeed;
-  this.duration = duration;
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.shape = 'circle';
 }
 
-function speedVSduration(result)
-        {
-          if(result != null && result != undefined)
-          {
-            var speed = [];
-            for(var i = 0; i < result.length; ++i)
-            {
-              var results = result[i];
-              speed.push(new barChart(results.avspeed, results.duration));
-            }
+function speedVSduration(result) {
+  if (result != null && result != undefined)
+  {
+    var circle =[];
 
-            speed.sort(function(a,b){
-              return a.duration - b.duration;
-            })
+    for (var i = 0; i < result.length; ++i)
+    {
+      var results = result[i];
 
-            var top5trips = [];
-            for(var i = 0; i < 5; ++i)
-            {
-              top5trips.push(speed[i]);
-            }
+      circle.push(new Circles(results.duration/60, results.avspeed, 5));
+    }
 
-            var speedData = [{
-              key: "duration",
-              values: top5trips
-            }];
-            console.log(speedData);
-             nv.addGraph(function() {
-                var chart = nv.models.discreteBarChart()
-                    .x(function(d) { return round(d.duration, 1) })
-                    .y(function(d) { return d.avspeed })
-                    .staggerLabels(true)
-                    //.staggerLabels(historicalBarChart[0].values.length > 8)
-                    .showValues(true)
-                    .duration(250)
-                    ;
+    
 
-                d3.select('#chart1')
-                    .attr('width', 300)
-                    .attr('height', 300)
-                    .datum(speedData)
-                    .call(chart);
+    var circleData = [{
+      key: "avspeed",
+      values: circle
+    }];
+    console.log(circleData);
+    nv.addGraph(function() {
+      var chart = nv.models.scatterChart()
+          .showDistX(true)    //showDist, when true, will display those little distribution lines on the axis.
+          .showDistY(true)
+          .color(d3.scale.category10().range());
 
-                nv.utils.windowResize(chart.update);
-                return chart;
-            });
-          }
-           
-        }
+
+    //Axis settings
+    chart.xAxis.tickFormat(d3.format('.0f'));
+    chart.yAxis.tickFormat(d3.format('.0f'));
+
+    //Axis labels
+    chart.xAxis.axisLabel('Duration (mins)');
+    chart.yAxis.axisLabel('Ave Speed (km)');
+
+    //Hide legend
+    chart.showLegend(false);
+
+    //Tooltip format
+    chart.tooltip.valueFormatter(d3.format('.02f'));
+
+    /*chart.tooltip.valueFormatter(function (d, i) {
+        return round(d, 1) + ' km';
+    });*/
+
+    //var myData = randomData(4,40);
+    d3.select('#chart1')
+      .attr('width', 400)
+      .attr('height', 400)
+      .datum(circleData)
+      .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+    });
+
+  }
+}
