1f929083126914b3b8268ae1096ffdfa041da5ef
Drengksapr <mfrance4@kent.edu>
2017-05-09 02:21:50 -0400

    Update iViz5.js

--- Code/Project/js/iViz5.js
+++ Code/Project/js/iViz5.js
@@ -4,42 +4,42 @@ function round(value, decimals) {
 
 function barChart(distance, avspeed)
 {
-  this.distance = distance;
-  this.avspeed = avspeed;
+  this.distance = distance;
+  this.avspeed = avspeed;
 }
 
 function distVSspeed(result)
-        {
-          if(result != null && result != undefined)
-          {
-            var speed = [];
-            for(var i = 0; i < result.length; ++i)
-            {
-              var results = result[i];
-              speed.push(new barChart(results.avspeed, results.distance/1000 );
-            }
+        {
+          if(result != null && result != undefined)
+          {
+            var speed = [];
+            for(var i = 0; i < result.length; ++i)
+            {
+              var results = result[i];
+              speed.push(new barChart(results.avspeed, results.distance/1000));
+            }
 
-            speed.sort(function(a,b){
-              return a.distance - b.distance;
-            })
+            speed.sort(function(a,b){
+              return a.distance - b.distance;
+            })
 
-            var top10trips = [];
-            for(var i = 0; i < 10; ++i)
-            {
-              top10trips.push(speed[i]);
-            }
+            var top10trips = [];
+            for(var i = 0; i < 10; ++i)
+            {
+              top10trips.push(speed[i]);
+            }
 
-            var speedData = [{
-              key: "Distance",
-              values: top10trips
-            }];
+            var speedData = [{
+              key: "Distance",
+              values: top10trips
+            }];
 
-            console.log(speedData);
-             nv.addGraph(function() {
-                var distVSspeedChart = nv.models.discreteBarChart()
-                    .x(function(d) { return round(d.distance, 1) })
-                    .y(function(d) { return d.avspeed/10 })
-                    .staggerLabels(true)
+            console.log(speedData);
+             nv.addGraph(function() {
+                var distVSspeedChart = nv.models.discreteBarChart()
+                    .x(function(d) { return round(d.distance, 1) })
+                    .y(function(d) { return d.avspeed })
+                    .staggerLabels(true)
                 //.staggerLabels(historicalBarChart[0].values.length > 8)
                     .showValues(true)
                     .duration(250)
