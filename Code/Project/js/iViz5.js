769805c494ffe459d843dccec87cb14aa1f9ec0a
Drengksapr <mfrance4@kent.edu>
2017-05-09 02:34:32 -0400

    Update iViz5.js

--- Code/Project/js/iViz5.js
+++ Code/Project/js/iViz5.js
@@ -16,7 +16,7 @@ function distVSspeed(result)
             for(var i = 0; i < result.length; ++i)
             {
               var results = result[i];
-              speed.push(new barChart(results.avspeed*10, results.distance/1000));
+              speed.push(new barChart(results.avspeed*10, results.distance));
             }
 
             speed.sort(function(a,b){
